420d0300f0c124cb0ae24943d4efb6da57b50be6
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-07-30 14:59:48 +0530

    Adds NLP Module

--- bin/run.js
+++ bin/run.js
@@ -5,10 +5,12 @@ const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
 
+const witToken = 'NVCOIJYZKHKS43NHEYLMAEJM256L2FWI';
+const witClient = require("../server/witClient")(witToken);
 const slackToken = 'xoxb-331044129858-406541948145-I7KyN4DwVxfjoYbYVa1zIpJ8';
 const slackLogLevel = 'info';
 
-const rtm = slackClient.init(slackToken, slackLogLevel);
+const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
 rtm.start();
 
 slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
--- server/slackClient.js
+++ server/slackClient.js
@@ -2,6 +2,8 @@
 let rtm = null;
 const { RTMClient } = require('@slack/client');
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+let nlp = null;
+
 
 function handleOnAuthenticated(rtmStartData) {
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
@@ -12,14 +14,32 @@ function addAuthenticatedHandler(rtm, handler) {
 }
 
 function handleOnMessage(message){
-    console.log(message);
-    rtm.sendMessage('This is a test msg', 'C9QEDGP5F', function messageSend(){
+    nlp.ask(message.text, (err, res) => {
+        if(err){
+            console.log(err);
+            return;
+        }
 
-    } )
+        if(!res.intent) {
+            return  rtm.sendMessage('Sorry, I did not understand.', 'C9QEDGP5F', function messageSend(){
+
+            });
+        }else if(res.intent[0].value == 'time' && res.location){
+            return  rtm.sendMessage(`I don't yet know the time in ${res.location[0].value}`, 'C9QEDGP5F', function messageSend(){
+
+            });
+        }else{
+            console.log(res);
+            rtm.sendMessage('Sorry, I did not understand.', 'C9QEDGP5F', function messageSend(){
+
+            } )
+        }
+    });
 }
 
-module.exports.init = function slackClient(token, logLevel) {
+module.exports.init = function slackClient(token, logLevel, nlpClient) {
     rtm = new RTMClient(token, {logLevel: logLevel});
+    nlp = nlpClient;
     addAuthenticatedHandler(rtm, handleOnAuthenticated);
     rtm.on('message', handleOnMessage);
     return rtm;
