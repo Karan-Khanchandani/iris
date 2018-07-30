1245850601e00f9dc216ca3be284a861b0335ef0
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-07-30 15:20:24 +0530

    Adds intents in app

--- server/slackClient.js
+++ server/slackClient.js
@@ -1,6 +1,8 @@
 'use strict';
 let rtm = null;
-const { RTMClient } = require('@slack/client');
+const {
+    RTMClient
+} = require('@slack/client');
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 let nlp = null;
 
@@ -13,32 +15,62 @@ function addAuthenticatedHandler(rtm, handler) {
     rtm.on('authenticated', handler);
 }
 
-function handleOnMessage(message){
-    nlp.ask(message.text, (err, res) => {
-        if(err){
-            console.log(err);
-            return;
-        }
+function handleOnMessage(message) {
 
-        if(!res.intent) {
-            return  rtm.sendMessage('Sorry, I did not understand.', 'C9QEDGP5F', function messageSend(){
+    if (message.text.toLowerCase().includes('karan-bot')) {
+        nlp.ask(message.text, (err, res) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
 
-            });
-        }else if(res.intent[0].value == 'time' && res.location){
-            return  rtm.sendMessage(`I don't yet know the time in ${res.location[0].value}`, 'C9QEDGP5F', function messageSend(){
+            try {
+                if(!res.intent || !res.intent[0] ||  !res.intent[0].value){
+                    throw new Error('Could not extract intent.')
+                }
 
-            });
-        }else{
-            console.log(res);
-            rtm.sendMessage('Sorry, I did not understand.', 'C9QEDGP5F', function messageSend(){
+                const intent = require('./intents' + res.intent[0].value + 'Intent');
 
-            } )
-        }
-    });
+                intent.process(res, function (err, response){
+                    if(error){
+                        console.log(error.message);
+                        return;
+                    }
+
+                    return rtm.sendMessage(response, 'C9QEDGP5F', function messageSend() {
+
+                    });
+                })
+            } catch (error) {
+                console.log(error);
+                console.log(response);
+                return rtm.sendMessage("Sorry, I don't know what you're talking about.", 'C9QEDGP5F', function messageSend() {
+
+                });
+            }
+            if (!res.intent) {
+                return rtm.sendMessage('Sorry, I did not understand.', 'C9QEDGP5F', function messageSend() {
+
+                });
+            } else if (res.intent[0].value == 'time' && res.location) {
+                return rtm.sendMessage(`I don't yet know the time in ${res.location[0].value}`, 'C9QEDGP5F', function messageSend() {
+
+                });
+            } else {
+                console.log(res);
+                rtm.sendMessage('Sorry, I did not understand.', 'C9QEDGP5F', function messageSend() {
+
+                })
+            }
+        });
+    }
+
 }
 
 module.exports.init = function slackClient(token, logLevel, nlpClient) {
-    rtm = new RTMClient(token, {logLevel: logLevel});
+    rtm = new RTMClient(token, {
+        logLevel: logLevel
+    });
     nlp = nlpClient;
     addAuthenticatedHandler(rtm, handleOnAuthenticated);
     rtm.on('message', handleOnMessage);
